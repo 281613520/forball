ef3e20c7b685ac5d57c88716e36fda052ab8f7c2
281613520 <281613520@qq.com>
2017-05-26 22:35:27 +0800

    boot相关

--- boot/src/main/java/com/xw/boot/Boot.java
+++ boot/src/main/java/com/xw/boot/Boot.java
@@ -1,11 +1,8 @@
-package com.xw;
+package com.xw.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by AnKh on 2017/5/24.
