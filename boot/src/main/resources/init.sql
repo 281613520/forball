f0db5b200520af834f31098d05c5d546605279bd
281613520 <281613520@qq.com>
2017-05-28 23:19:07 +0800

    sql

---
+++ boot/src/main/resources/init.sql
@@ -0,0 +1,69 @@
+DROP TABLE IF EXISTS `tb_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_admin` (
+  `id` varchar(255) NOT NULL,
+  `is_delete` bit(1) NOT NULL,
+  `operation_time` datetime DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_article`
+--
+
+DROP TABLE IF EXISTS `tb_article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_article` (
+  `id` varchar(255) NOT NULL,
+  `is_delete` bit(1) NOT NULL,
+  `operation_time` datetime DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `down` int(11) NOT NULL,
+  `is_top` bit(1) NOT NULL,
+  `read_num` int(11) NOT NULL,
+  `tag_id` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `up` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_module`
+--
+
+DROP TABLE IF EXISTS `tb_module`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_module` (
+  `id` varchar(255) NOT NULL,
+  `is_delete` bit(1) NOT NULL,
+  `operation_time` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_tag`
+--
+
+DROP TABLE IF EXISTS `tb_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_tag` (
+  `id` varchar(255) NOT NULL,
+  `is_delete` bit(1) NOT NULL,
+  `operation_time` datetime DEFAULT NULL,
+  `article_num` int(11) NOT NULL,
+  `module_id` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
