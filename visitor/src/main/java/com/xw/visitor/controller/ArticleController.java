5de6dfe56ff9244a07c8e5ac1615e9ce00ff3b7f
281613520 <281613520@qq.com>
2017-06-09 16:46:26 +0800

    。

--- visitor/src/main/java/com/xw/visitor/controller/ArticleController.java
+++ visitor/src/main/java/com/xw/visitor/controller/ArticleController.java
@@ -1,7 +1,19 @@
 package com.xw.visitor.controller;
 
+import com.xw.visitor.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Ankh on 2017/6/9.
  */
+@RestController
 public class ArticleController {
+    @Autowired
+    ArticleService articleService;
+
+    //新增
+    //删除
+    //查找
+    //按照tag分页查找
 }
--- visitor/src/main/java/com/xw/visitor/controller/TagController.java
+++ visitor/src/main/java/com/xw/visitor/controller/TagController.java
@@ -1,7 +1,47 @@
 package com.xw.visitor.controller;
 
+import com.xw.commons.response.ReponseTemplate;
+import com.xw.commons.response.StatusCode;
+import com.xw.visitor.entity.Tag;
+import com.xw.visitor.service.ModuleService;
+import com.xw.visitor.service.TagService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Ankh on 2017/6/9.
  */
+@Slf4j
+@RestController(value = "/tag")
 public class TagController {
+    @Autowired
+    TagService tagService;
+    @Autowired
+    ModuleService moduleService;
+
+    @PostMapping(value = "/new")
+    public Object addTag(Tag tag){
+        if (!isTagValid(tag)){
+
+        }
+
+        tagService.save(tag);
+        return new ReponseTemplate(StatusCode.SUCCESS);
+    }
+
+    private boolean isTagValid(final Tag tag) {
+        if(tag == null){
+            return false;
+        }
+        if(!StringUtils.hasText(tag.)) {
+            return false;
+        }
+        if(!StringUtils.hasText(tag.getModuleId())) {
+            return false;
+        }
+        return moduleService.findById(tag.getModuleId()) != null;
+    }
 }
