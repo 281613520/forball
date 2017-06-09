2f2f06a5528eef75d4fdcdfd73ef9f199532bf65
281613520 <281613520@qq.com>
2017-06-09 16:46:09 +0800

    。

--- visitor/src/main/java/com/xw/visitor/controller/ModuleController.java
+++ visitor/src/main/java/com/xw/visitor/controller/ModuleController.java
@@ -1,7 +1,15 @@
 package com.xw.visitor.controller;
 
+import com.xw.visitor.service.ModuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Ankh on 2017/6/9.
  */
+@RestController
 public class ModuleController {
+    @Autowired
+    ModuleService moduleService;
+    //新增，删除，查找
 }
