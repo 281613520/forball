68177ccef188d0ca34856b4c3fb8beecfb0b7814
281613520 <281613520@qq.com>
2017-07-25 23:17:51 +0800

    oauth有一点点懂了

--- user/src/main/java/com/xw/user/oauth/SecurityConfig.java
+++ user/src/main/java/com/xw/user/oauth/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.xw.user.oauth;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 /**
- * Created by Ankh on 2017/7/20.
+ * Created by Ankh on 2017/7/25.
  */
+@Slf4j
 @Configuration
-public class SecurityConfig {
-
+@EnableAuthorizationServer
+@EnableOAuth2Client
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 }
